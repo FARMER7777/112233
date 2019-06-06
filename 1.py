c4269ebeffa71331ce2dec232d3532bb3f49a448
Farmer <sceh7777@gmail.com>
2019-06-06 18:42:47 +0300

    112233

---
+++ 1.py
@@ -0,0 +1,11 @@
+from turtle import*
+
+
+fd(50)
+left(120)
+fd(50)
+left(120)
+fd(50)
+circle(100)
+
+exitonclick()
---
+++ 2.py
@@ -0,0 +1,19 @@
+from turtle import*
+
+speed(315487628543176518643587614)
+
+for i in range(180):
+	forward(100)
+	right(30)
+	forward(20)
+	right(60)
+	forward(50)
+	right(30)
+
+	penup()
+	setposition(0,1)
+	pendown()
+
+	right(1)
+
+exitonclick()	
